24d28d4db92839d7790bd194a03d7659b8a79adc
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-01 14:59:53 +0200

    Provide new injector methods

--- app/src/main/java/com/yurii/foody/utils/Injector.kt
+++ app/src/main/java/com/yurii/foody/utils/Injector.kt
@@ -2,16 +2,32 @@ package com.yurii.foody.utils
 
 import android.content.Context
 import com.yurii.foody.api.Service
+import com.yurii.foody.api.UserRoleEnum
 import com.yurii.foody.authorization.AuthorizationRepository
+import com.yurii.foody.authorization.confirmation.ConfirmationViewModel
+import com.yurii.foody.authorization.loading.LoadingViewModel
 import com.yurii.foody.authorization.login.LogInViewModel
+import com.yurii.foody.authorization.role.ChooseRoleViewModel
 
 object Injector {
 
-    private fun provideAuthorizationRepository(context: Context) = AuthorizationRepository(
+    private fun provideAuthorizationRepository(context: Context) = AuthorizationRepository.create(
         authDataStorage = AuthDataStorage.create(context),
-        apiTokenAuth = Service.authService
+        api = Service
     )
 
-    fun provideLogInViewModel(context: Context): LogInViewModel.Factory =
-        LogInViewModel.Factory(repository = provideAuthorizationRepository(context))
+    fun provideChooseRoleViewModel(context: Context, role: UserRoleEnum, selectNewRole: Boolean, isUserRoleConfirmed: Boolean) =
+        ChooseRoleViewModel.Factory(
+            repository = provideAuthorizationRepository(context),
+            userRoleEnum = role,
+            selectNewRole = selectNewRole,
+            isUserRoleConfirmed
+        )
+
+    fun provideLogInViewModel(context: Context) = LogInViewModel.Factory(repository = provideAuthorizationRepository(context))
+
+    fun provideLoadingViewModel(context: Context) = LoadingViewModel.Factory(repository = provideAuthorizationRepository(context))
+
+    fun provideConfirmationViewModel(context: Context, userIsNotConfirmed: Boolean, role: UserRoleEnum) =
+        ConfirmationViewModel.Factory(provideAuthorizationRepository(context), userIsNotConfirmed, role)
 }

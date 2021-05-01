1c5d6a3b586aafce04e64b20f80b63562a4e0cde
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-01 12:58:50 +0200

    Create: User data class, UserRole enum, Pagination

--- app/src/main/java/com/yurii/foody/api/Data.kt
+++ app/src/main/java/com/yurii/foody/api/Data.kt
@@ -2,7 +2,28 @@ package com.yurii.foody.api
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import java.util.*
 import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
+
+enum class UserRoleEnum(val role: String) {
+    @Json(name = "client")
+    CLIENT("client"),
+
+    @Json(name = "executor")
+    EXECUTOR("executor"),
+
+    @Json(name = "administrator")
+    ADMINISTRATOR("administrator");
+
+    companion object {
+        fun toEnum(value: String) = valueOf(value.toUpperCase(Locale.getDefault()))
+    }
+}
+
+@JsonClass(generateAdapter = true)
+data class Pagination<T>(val count: Int, val next: String?, val previous: String?, val results: List<T>)
+
+data class UserRole(@Json(name = "user") val userId: Int, @Json(name = "is_confirmed") val isConfirmed: Boolean, val role: UserRoleEnum)
 
 @JsonClass(generateAdapter = true)
 data class AuthData(val username: String, val password: String)
@@ -11,4 +32,11 @@ data class AuthData(val username: String, val password: String)
 data class AuthResponseData(val token: String, @Json(name = "user_id") val userId: Int, val email: String)
 
 @JsonClass(generateAdapter = true)
-data class Error(val error: JvmType.Object)
+data class User(
+    @Json(name = "pk") val id: Int,
+    val email: String,
+    @Json(name = "first_name") val firstName: String,
+    @Json(name = "last_name") val lastName: String,
+    @Json(name = "phone_number") val phoneNumber: String,
+    @Json(name = "is_email_confirmed") val isEmailConfirmed: Boolean
+)

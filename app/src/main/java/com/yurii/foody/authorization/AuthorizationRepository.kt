66991cdcdf6925128b07c447643efd77d1a5b5d7
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-01 14:59:33 +0200

    Add new methods to repository

--- app/src/main/java/com/yurii/foody/authorization/AuthorizationRepository.kt
+++ app/src/main/java/com/yurii/foody/authorization/AuthorizationRepository.kt
@@ -1,18 +1,16 @@
 package com.yurii.foody.authorization
 
 import com.haroldadmin.cnradapter.NetworkResponse
-import com.yurii.foody.api.ApiTokenAuth
-import com.yurii.foody.api.AuthData
-import com.yurii.foody.api.AuthResponseData
+import com.yurii.foody.api.*
 import com.yurii.foody.utils.AuthDataStorage
 import com.yurii.foody.utils.toAuthDataStorage
 
 class AuthorizationRepository private constructor(
     private val authDataStorage: AuthDataStorage,
-    private val apiTokenAuth: ApiTokenAuth
+    private val api: Service
 ) {
     suspend fun logIn(authData: AuthData): NetworkResponse<AuthResponseData, Unit> {
-        val response = apiTokenAuth.logIn(authData)
+        val response = api.authService.logIn(authData)
 
         if (response is NetworkResponse.Success)
             saveAuthCredentials(response.body)
@@ -20,13 +18,27 @@ class AuthorizationRepository private constructor(
         return response
     }
 
+    fun setToken(token: String) = api.createAuthenticatedService(token)
+
+    suspend fun clearUserAuth() = authDataStorage.clearUserAuth()
+
+    suspend fun getUser(id: Long) = api.usersService.getUser(id)
+
+    suspend fun getUsersRoles(userId: Int? = null) = api.usersService.getUsersRoles(userId)
+
+    fun getLogInAuthenticatedDataFlow() = authDataStorage.authData
+
+    fun getSelectedUserRoleFlow() = authDataStorage.selectedRole
+
+    suspend fun saveUserRole(userRoleEnum: UserRoleEnum) = authDataStorage.saveUserRole(userRoleEnum)
+
     private suspend fun saveAuthCredentials(authResponseData: AuthResponseData) = authDataStorage.save(authResponseData.toAuthDataStorage())
 
     companion object {
         private var INSTANCE: AuthorizationRepository? = null
-        fun create(authDataStorage: AuthDataStorage, apiTokenAuth: ApiTokenAuth): AuthorizationRepository {
+        fun create(authDataStorage: AuthDataStorage, api: Service): AuthorizationRepository {
             if (INSTANCE == null)
-                synchronized(AuthorizationRepository::class.java) { INSTANCE = AuthorizationRepository(authDataStorage, apiTokenAuth) }
+                synchronized(AuthorizationRepository::class.java) { INSTANCE = AuthorizationRepository(authDataStorage, api) }
 
             return INSTANCE!!
         }

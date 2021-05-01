9d71312f986a4878304f7b459640869053c260cc
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-01 12:50:08 +0200

    Create Loading Fragment

--- app/src/main/java/com/yurii/foody/authorization/loading/LoadingFragment.kt
+++ app/src/main/java/com/yurii/foody/authorization/loading/LoadingFragment.kt
@@ -1,22 +1,27 @@
 package com.yurii.foody.authorization.loading
 
-import android.animation.Animator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.airbnb.lottie.LottieAnimationView
 import com.yurii.foody.R
+import com.yurii.foody.api.UserRoleEnum
+import com.yurii.foody.authorization.confirmation.ConfirmationFragment
 import com.yurii.foody.databinding.FragmentLoadingBinding
-import timber.log.Timber
+import com.yurii.foody.ui.ErrorDialog
+import com.yurii.foody.utils.Injector
+import com.yurii.foody.utils.observeOnLifecycle
+import com.yurii.foody.utils.setListener
+import com.yurii.foody.utils.statusBar
 
 class LoadingFragment : Fragment() {
-
+    private val viewModel: LoadingViewModel by viewModels { Injector.provideLoadingViewModel(requireContext()) }
     private lateinit var binding: FragmentLoadingBinding
+    private val errorDialog: ErrorDialog by lazy { ErrorDialog(requireContext()) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_loading, container, false)
@@ -25,25 +30,31 @@ class LoadingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.animation.setListener { observeEvents() }
+    }
 
-        binding.animation.addAnimatorListener(object : Animator.AnimatorListener {
-            override fun onAnimationStart(p0: Animator?) {
-                Timber.i("Animation Start")
-            }
+    private fun observeEvents() = viewModel.eventFlow.observeOnLifecycle(viewLifecycleOwner) {
+        when (it) {
+            is LoadingViewModel.Event.NavigateToAuthenticationScreen -> navigateToAuthenticationScreen()
+            is LoadingViewModel.Event.NavigateToChooseRoleScreen -> navigateToChooseRoleScreen(it.role)
+            is LoadingViewModel.Event.NetworkError -> errorDialog.show("Network error ${it.message}")
+            is LoadingViewModel.Event.ServerError -> errorDialog.show("Server error ${it.code}")
+            is LoadingViewModel.Event.UnknownError -> errorDialog.show("Unknown error ${it.message}")
+            is LoadingViewModel.Event.NavigateToUserIsNotConfirmedScreen -> navigateToConfirmationScreen(ConfirmationFragment.Mode.CONFIRMATION_EMAIL)
+            is LoadingViewModel.Event.NavigateToUserRoleIsNotConfirmedScreen -> navigateToConfirmationScreen(ConfirmationFragment.Mode.CONFIRMATION_EXECUTOR_REQUEST)
+        }
+    }
 
-            override fun onAnimationEnd(p0: Animator?) {
-                Timber.i("Animation End")
-            }
+    private fun navigateToAuthenticationScreen() {
+        findNavController().navigate(LoadingFragmentDirections.actionLoadingFragmentToAuthenticationFragment())
+    }
 
-            override fun onAnimationCancel(p0: Animator?) {
+    private fun navigateToChooseRoleScreen(role: UserRoleEnum) {
+        findNavController().navigate(LoadingFragmentDirections.actionLoadingFragmentToChooseRoleFragment(role))
+    }
 
-            }
-
-            override fun onAnimationRepeat(p0: Animator?) {
-                findNavController().navigate(R.id.action_loadingFragment_to_authenticationFragment)
-            }
-
-        })
+    private fun navigateToConfirmationScreen(mode: ConfirmationFragment.Mode) {
+        findNavController().navigate(LoadingFragmentDirections.actionLoadingFragmentToConfirmationFragment(mode))
     }
 
     override fun onResume() {
@@ -51,20 +62,8 @@ class LoadingFragment : Fragment() {
         statusBar(hide = true)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
+    override fun onStop() {
+        super.onStop()
         statusBar(hide = false)
-    }
-
-    private fun statusBar(hide: Boolean) {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-            activity?.window?.setDecorFitsSystemWindows(!hide)
-        } else {
-            @Suppress("DEPRECATION")
-            if (hide)
-                activity?.window?.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-            else
-                activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        }
     }
 }
---
+++ app/src/main/java/com/yurii/foody/authorization/loading/LoadingViewModel.kt
@@ -0,0 +1,86 @@
+package com.yurii.foody.authorization.loading
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.haroldadmin.cnradapter.NetworkResponse
+import com.yurii.foody.api.User
+import com.yurii.foody.api.UserRoleEnum
+import com.yurii.foody.authorization.AuthorizationRepository
+import com.yurii.foody.utils.AuthDataStorage
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import java.net.HttpURLConnection.HTTP_UNAUTHORIZED
+
+class LoadingViewModel(private val repository: AuthorizationRepository) : ViewModel() {
+    sealed class Event {
+        data class NavigateToChooseRoleScreen(val role: UserRoleEnum) : Event()
+        data class ServerError(val code: Int) : Event()
+        data class NetworkError(val message: String?) : Event()
+        data class UnknownError(val message: String?) : Event()
+        object NavigateToAuthenticationScreen : Event()
+        object NavigateToUserIsNotConfirmedScreen : Event()
+        object NavigateToUserRoleIsNotConfirmedScreen : Event()
+    }
+
+    private val eventChannel = Channel<Event>()
+    val eventFlow = eventChannel.receiveAsFlow()
+
+    init {
+        viewModelScope.launch {
+            repository.getLogInAuthenticatedDataFlow().catch {
+                eventChannel.send(Event.NavigateToAuthenticationScreen)
+            }.collect {
+                checkAuthorization(it)
+            }
+        }
+    }
+
+    private suspend fun checkAuthorization(authData: AuthDataStorage.Data) {
+        repository.setToken(authData.token)
+        when (val response = repository.getUser(authData.userId.toLong())) {
+            is NetworkResponse.Success -> checkEmailConfirmation(response.body, authData)
+            is NetworkResponse.ServerError -> eventChannel.send(
+                if (response.code == HTTP_UNAUTHORIZED) Event.NavigateToAuthenticationScreen
+                else Event.ServerError(response.code)
+            )
+            is NetworkResponse.NetworkError -> eventChannel.send(Event.NetworkError(response.error.message))
+            is NetworkResponse.UnknownError -> eventChannel.send(Event.UnknownError(response.error.message))
+        }
+    }
+
+    private suspend fun checkEmailConfirmation(user: User, authData: AuthDataStorage.Data) {
+        if (user.isEmailConfirmed)
+            checkUserRoleConfirmation(authData)
+        else
+            eventChannel.send(Event.NavigateToUserIsNotConfirmedScreen)
+    }
+
+    private suspend fun checkUserRoleConfirmation(authData: AuthDataStorage.Data) {
+        when (val response = repository.getUsersRoles(userId = authData.userId)) {
+            is NetworkResponse.Success -> response.body.results.first().run {
+                if (isConfirmed)
+                    eventChannel.send(Event.NavigateToChooseRoleScreen(role))
+                else
+                    eventChannel.send(Event.NavigateToUserRoleIsNotConfirmedScreen)
+            }
+
+            is NetworkResponse.ServerError -> eventChannel.send(Event.ServerError(response.code))
+            is NetworkResponse.NetworkError -> eventChannel.send(Event.NetworkError(response.error.message))
+            is NetworkResponse.UnknownError -> eventChannel.send(Event.UnknownError(response.error.message))
+        }
+    }
+
+    class Factory(private val repository: AuthorizationRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(LoadingViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return LoadingViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewModel")
+        }
+    }
+}

fe125a0ca2083567c2f4a8fcb13224835ac01de0
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-01 12:57:27 +0200

    Add possibility to save UserRole. Also add method to clear data

--- app/src/main/java/com/yurii/foody/utils/Storage.kt
+++ app/src/main/java/com/yurii/foody/utils/Storage.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
+import com.yurii.foody.api.UserRoleEnum
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -20,6 +21,22 @@ class AuthDataStorage private constructor(private val dataStore: DataStore<Prefe
         )
     }
 
+    val selectedRole: Flow<UserRoleEnum> = dataStore.data.map { preferences ->
+        UserRoleEnum.toEnum(preferences[KEY_USER_ROLE]!!)
+    }
+
+    suspend fun clearUserAuth() {
+        dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+
+    suspend fun saveUserRole(role: UserRoleEnum) {
+        dataStore.edit { preferences ->
+            preferences[KEY_USER_ROLE] = role.role
+        }
+    }
+
     suspend fun save(data: Data) {
         dataStore.edit { preferences ->
             preferences[KEY_TOKEN] = data.token
@@ -32,6 +49,7 @@ class AuthDataStorage private constructor(private val dataStore: DataStore<Prefe
         private val KEY_TOKEN = stringPreferencesKey("key_token")
         private val KEY_EMAIL = stringPreferencesKey("key_email")
         private val KEY_USER_ID = intPreferencesKey("key_user_id")
+        private val KEY_USER_ROLE = stringPreferencesKey("key_user_role")
 
         private var INSTANCE: AuthDataStorage? = null
 

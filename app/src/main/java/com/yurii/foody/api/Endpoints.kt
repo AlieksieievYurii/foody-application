2f429c8e39eedd62d60a2b8c645b94a136156f90
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-01 13:00:21 +0200

    Add new endpoints: Get user detail and get userroles. Also implement authentication

--- app/src/main/java/com/yurii/foody/api/Endpoints.kt
+++ app/src/main/java/com/yurii/foody/api/Endpoints.kt
@@ -1,10 +1,18 @@
 package com.yurii.foody.api
 
 import com.haroldadmin.cnradapter.NetworkResponse
-import retrofit2.http.Body
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface ApiTokenAuth {
     @POST("api-token-auth/")
     suspend fun logIn(@Body authData: AuthData): NetworkResponse<AuthResponseData, Unit>
+}
+
+interface ApiUsers {
+
+    @GET("/users/{id}")
+    suspend fun getUser(@Path("id") id: Long): NetworkResponse<User, Unit>
+
+    @GET("/users/roles")
+    suspend fun getUsersRoles(@Query("user") userId: Int? = null): NetworkResponse<Pagination<UserRole>, Unit>
 }
--- app/src/main/java/com/yurii/foody/api/Service.kt
+++ app/src/main/java/com/yurii/foody/api/Service.kt
@@ -4,6 +4,7 @@ import com.haroldadmin.cnradapter.NetworkResponseAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.yurii.foody.Application
+import okhttp3.*
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -20,5 +21,29 @@ object Service {
             .build()
     }
 
-    val authService by lazy { service.create(ApiTokenAuth::class.java) }
+    private var authenticatedService: Retrofit? = null
+
+    fun createAuthenticatedService(token: String) {
+        authenticatedService = service.newBuilder()
+            .client(createHttpClient(token))
+            .build()
+    }
+
+    private fun authenticatedService(): Retrofit {
+        checkNotNull(authenticatedService) { "You must call createAuthenticatedService firs" }
+        return authenticatedService!!
+    }
+
+    private const val HEADER_AUTHORIZATION = "Authorization"
+
+    private fun createHttpClient(token: String) = OkHttpClient.Builder()
+        .addInterceptor {
+            val newRequest = it.request().newBuilder()
+                .header(HEADER_AUTHORIZATION, "Bearer $token")
+                .build()
+            it.proceed(newRequest)
+        }.build()
+
+    val authService: ApiTokenAuth by lazy { service.create(ApiTokenAuth::class.java) }
+    val usersService: ApiUsers by lazy { authenticatedService().create(ApiUsers::class.java) }
 }

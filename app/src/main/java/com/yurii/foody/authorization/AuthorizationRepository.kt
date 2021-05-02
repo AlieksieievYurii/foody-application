cfb6d3924f11fb945bac822669d5935a758ae1ce
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-02 16:52:36 +0200

    Adapt repository to the previous changes

--- app/src/main/java/com/yurii/foody/authorization/AuthorizationRepository.kt
+++ app/src/main/java/com/yurii/foody/authorization/AuthorizationRepository.kt
@@ -1,36 +1,44 @@
 package com.yurii.foody.authorization
 
-import com.haroldadmin.cnradapter.NetworkResponse
 import com.yurii.foody.api.*
 import com.yurii.foody.utils.AuthDataStorage
 import com.yurii.foody.utils.toAuthDataStorage
+import kotlinx.coroutines.flow.*
 
 class AuthorizationRepository private constructor(
     private val authDataStorage: AuthDataStorage,
     private val api: Service
 ) {
-    suspend fun logIn(authData: AuthData): NetworkResponse<AuthResponseData, Unit> {
-        val response = api.authService.logIn(authData)
-
-        if (response is NetworkResponse.Success)
-            saveAuthCredentials(response.body)
-
-        return response
-    }
+    suspend fun logIn(authData: AuthData): Flow<AuthResponseData> =
+        Service.asFlow { api.authService.logIn(authData) }.map {
+            saveAuthCredentials(it)
+            api.createAuthenticatedService(it.token)
+            it
+        }
 
     fun setToken(token: String) = api.createAuthenticatedService(token)
 
     suspend fun clearUserAuth() = authDataStorage.clearUserAuth()
 
-    suspend fun getUser(id: Long) = api.usersService.getUser(id)
+    suspend fun getUser(id: Long) = Service.asFlow { api.usersService.getUser(id) }
 
-    suspend fun getUsersRoles(userId: Int? = null) = api.usersService.getUsersRoles(userId)
+    suspend fun getUsersRoles(userId: Int? = null) = Service.asFlow { api.usersService.getUsersRoles(userId) }
 
     fun getLogInAuthenticatedDataFlow() = authDataStorage.authData
 
-    fun getSelectedUserRoleFlow() = authDataStorage.selectedRole
+    suspend fun getSelectedUserRole() = authDataStorage.selectedRole.first()
+
+    suspend fun getUserRole(): UserRoleEnum? = authDataStorage.userRole.first()
+
+    suspend fun isUserRoleConfirmed() = authDataStorage.isRoleConfirmed.first()
 
     suspend fun saveUserRole(userRoleEnum: UserRoleEnum) = authDataStorage.saveUserRole(userRoleEnum)
+
+    suspend fun saveSelectedUserRole(userRoleEnum: UserRoleEnum) = authDataStorage.saveSelectedUserRole(userRoleEnum)
+
+    suspend fun clearCurrentSelectedUserRole() = authDataStorage.clearCurrentSelectedUserRole()
+
+    suspend fun setUserRoleStatus(isConfirmed: Boolean) = authDataStorage.seUserRoleStatus(isConfirmed)
 
     private suspend fun saveAuthCredentials(authResponseData: AuthResponseData) = authDataStorage.save(authResponseData.toAuthDataStorage())
 

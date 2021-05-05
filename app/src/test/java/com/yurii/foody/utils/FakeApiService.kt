a9506cfc644766dc5613d61d1108047998692ead
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-05 20:13:06 +0200

    Cover little bit LoadingViewModel with testcases

--- app/src/test/java/com/yurii/foody/utils/FakeApiService.kt
+++ app/src/test/java/com/yurii/foody/utils/FakeApiService.kt
@@ -1,6 +1,11 @@
 package com.yurii.foody.utils
 
 import com.yurii.foody.api.*
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import retrofit2.HttpException
+import retrofit2.Response
+import java.net.HttpURLConnection
 import java.net.HttpURLConnection.HTTP_NOT_FOUND
 
 class FakeApiService(
@@ -8,7 +13,9 @@ class FakeApiService(
     val userRoles: MutableList<UserRole> = mutableListOf()
 ) : ApiServiceInterface {
 
-    private var isAuthenticatedServiceCreated: Boolean = false
+    var doesGetUserThrowServerErrorUnauthorized: Boolean = false
+
+    var isAuthenticatedServiceCreated: Boolean = false
 
     override fun createAuthenticatedService(token: String) {
         isAuthenticatedServiceCreated = true
@@ -19,7 +26,7 @@ class FakeApiService(
 
     private inner class AuthService : ApiTokenAuth {
         override suspend fun logIn(authData: AuthData): AuthResponseData {
-            return AuthResponseData(token="1234", userId = 1, email = "test@email.com")
+            return AuthResponseData(token = "1234", userId = 1, email = "test@email.com")
         }
 
         override suspend fun registerUser(user: RegistrationForm): RegistrationForm {
@@ -29,6 +36,9 @@ class FakeApiService(
 
     private inner class UsersService : ApiUsers {
         override suspend fun getUser(id: Long): User {
+            if (doesGetUserThrowServerErrorUnauthorized)
+                throw HttpException(Response.error<Nothing>(HttpURLConnection.HTTP_UNAUTHORIZED, ResponseBody.create(MediaType.parse("application/json"), "")))
+
             return users.find { it.id.toLong() == id } ?: throw ResponseException.ServerError(
                 code = HTTP_NOT_FOUND,
                 responseMessage = "User with ID $id not found", null

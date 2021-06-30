0d8472c6a7b297518d1d72ea2bc40754e939e20e
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-06-30 15:50:27 +0200

    Refactor the dialog 'UploadDialog' so that new instances is always opened

--- app/src/main/java/com/yurii/foody/ui/UploadPhotoDialog.kt
+++ app/src/main/java/com/yurii/foody/ui/UploadPhotoDialog.kt
@@ -14,7 +14,134 @@ import com.yurii.foody.R
 import com.yurii.foody.databinding.DialogPhotoUploadBinding
 import java.lang.IllegalStateException
 
-class UploadPhotoDialog(private val context: Context, registry: ActivityResultRegistry) {
+class UploadPhotoDialog(private val context: Context, private val registry: ActivityResultRegistry) {
+    private class Dialog(private val context: Context, registry: ActivityResultRegistry) {
+        private var mode: Mode = Mode.EXTERNAL
+        private var imageUri: Uri? = null
+        private val binding: DialogPhotoUploadBinding by lazy {
+            DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.dialog_photo_upload, null, false)
+        }
+
+        private val uploadingPhotoIntent = registry.register("Image", ActivityResultContracts.GetContent()) { uri: Uri? ->
+            if (uri != null)
+                onImageIsSelected(uri)
+        }
+
+        private val dialog: AlertDialog by lazy {
+            AlertDialog.Builder(context)
+                .setView(binding.root)
+                .setTitle(R.string.label_photo_uploading)
+                .setPositiveButton(R.string.label_add, null)
+                .setNegativeButton(R.string.label_cancel) { _, _ -> }
+                .create()
+        }
+
+        init {
+            binding.photoType.setOnCheckedChangeListener { _, id ->
+                imageUri = null
+                mode = when (id) {
+                    R.id.external -> {
+                        showExternalUploadingOption()
+                        Mode.EXTERNAL
+                    }
+                    R.id.internal -> {
+                        showInternalUploadOption()
+                        Mode.INTERNAL
+                    }
+                    else -> throw IllegalStateException("Unhandled mode")
+                }
+            }
+        }
+
+        private fun onImageIsSelected(uri: Uri?) {
+            imageUri = uri
+            binding.apply {
+                preview.isVisible = true
+                preview.load(uri)
+                error.isVisible = false
+            }
+        }
+
+        private fun showExternalUploadingOption() {
+            binding.apply {
+                imageUrlLayout.isVisible = true
+                action.text = context.getText(R.string.label_upload)
+                action.setOnClickListener {
+                    preview.isVisible = true
+                    preview.load(binding.imageUrl.text.toString())
+                    error.isVisible = false
+                }
+
+                error.isVisible = false
+                preview.isVisible = false
+                imageUrl.setOnKeyListener { _, keyCode, keyEvent ->
+                    if (keyEvent.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                        binding.preview.apply {
+                            isVisible = true
+                            error.isVisible = false
+                            load(binding.imageUrl.text.toString()) {
+                                error(R.drawable.image_error_placeholder)
+                            }
+                        }
+                        return@setOnKeyListener true
+                    }
+                    false
+                }
+            }
+        }
+
+        private fun showInternalUploadOption() {
+            binding.apply {
+                preview.isVisible = false
+                imageUrlLayout.isVisible = false
+                error.isVisible = false
+                action.isVisible = true
+                action.text = context.getText(R.string.label_select)
+                action.setOnClickListener { uploadingPhotoIntent.launch("image/*") }
+            }
+        }
+
+        private fun showError(error: Error) {
+            binding.error.apply {
+                isVisible = true
+                text = context.getText(
+                    when (error) {
+                        Error.SELECT_IMAGE -> R.string.error_select_image
+                        Error.ENTER_IMAGE_URL -> R.string.error_enter_image_url
+                    }
+                )
+            }
+        }
+
+        private fun onExternalImageIsSelected(callback: (result: Result) -> Unit) {
+            val imageUrl = binding.imageUrl.text.toString()
+            if (imageUrl.isNotEmpty()) {
+                callback.invoke(Result.External(imageUrl))
+                dialog.dismiss()
+            } else
+                showError(Error.ENTER_IMAGE_URL)
+        }
+
+        private fun onInternalImageIsSelected(callback: (result: Result) -> Unit) {
+            if (imageUri != null) {
+                callback.invoke(Result.Internal(imageUri!!))
+                dialog.dismiss()
+            } else
+                showError(Error.SELECT_IMAGE)
+        }
+
+        fun show(callback: (result: Result) -> Unit) {
+            dialog.show()
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                when (mode) {
+                    Mode.EXTERNAL -> onExternalImageIsSelected(callback)
+                    Mode.INTERNAL -> onInternalImageIsSelected(callback)
+                }
+            }
+
+        }
+    }
+
     private enum class Mode { EXTERNAL, INTERNAL }
     private enum class Error { SELECT_IMAGE, ENTER_IMAGE_URL }
     sealed class Result {
@@ -22,114 +149,7 @@ class UploadPhotoDialog(private val context: Context, registry: ActivityResultRe
         data class Internal(val uri: Uri) : Result()
     }
 
-    private var mode: Mode = Mode.EXTERNAL
-    private var imageUri: Uri? = null
-    private val binding: DialogPhotoUploadBinding by lazy {
-        DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.dialog_photo_upload, null, false)
-    }
-
-    private val uploadingPhotoIntent = registry.register("Image", ActivityResultContracts.GetContent()) { uri: Uri? ->
-        imageUri = uri
-        binding.apply {
-            preview.isVisible = true
-            preview.load(uri)
-            error.isVisible = false
-        }
-    }
-
-    private val dialog: AlertDialog by lazy {
-        AlertDialog.Builder(context)
-            .setView(binding.root)
-            .setTitle(R.string.label_photo_uploading)
-            .setPositiveButton(R.string.label_add, null)
-            .setNegativeButton(R.string.label_cancel) { _, _ -> }
-            .create()
-    }
-
-    init {
-        binding.photoType.setOnCheckedChangeListener { _, id ->
-            mode = when (id) {
-                R.id.external -> {
-                    showExternalUploadingOption()
-                    Mode.EXTERNAL
-                }
-                R.id.internal -> {
-                    showInternalUploadOption()
-                    Mode.INTERNAL
-                }
-                else -> throw IllegalStateException("Unhandled mode")
-            }
-        }
-    }
-
-    private fun showExternalUploadingOption() {
-        binding.apply {
-            imageUrlLayout.isVisible = true
-            select.isVisible = false
-            error.isVisible = false
-            preview.isVisible = false
-            imageUrl.setOnKeyListener { _, keyCode, keyEvent ->
-                if (keyEvent.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                    binding.preview.apply {
-                        isVisible = true
-                        error.isVisible = false
-                        load(binding.imageUrl.text.toString()) {
-                            error(R.drawable.image_error_placeholder)
-                        }
-                    }
-                    return@setOnKeyListener true
-                }
-                false
-            }
-        }
-    }
-
-    private fun showInternalUploadOption() {
-        binding.apply {
-            preview.isVisible = false
-            imageUrlLayout.isVisible = false
-            error.isVisible = false
-            select.isVisible = true
-            select.setOnClickListener { uploadingPhotoIntent.launch("image/*") }
-        }
-    }
-
-    private fun showError(error: Error) {
-        binding.error.apply {
-            isVisible = true
-            text = context.getText(
-                when (error) {
-                    Error.SELECT_IMAGE -> R.string.error_select_image
-                    Error.ENTER_IMAGE_URL -> R.string.error_enter_image_url
-                }
-            )
-        }
-    }
-
     fun show(callback: (result: Result) -> Unit) {
-        imageUri = null
-        binding.preview.isVisible = false
-        dialog.show()
-        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-            when (mode) {
-                Mode.EXTERNAL -> {
-                    val imageUrl = binding.imageUrl.text.toString()
-                    if (imageUrl.isNotEmpty()) {
-                        callback.invoke(Result.External(imageUrl))
-                        dialog.cancel()
-                        dialog.dismiss()
-                    } else
-                        showError(Error.ENTER_IMAGE_URL)
-                }
-
-                Mode.INTERNAL -> {
-                    if (imageUri != null) {
-                        callback.invoke(Result.Internal(imageUri!!))
-                        dialog.dismiss()
-                    } else
-                        showError(Error.SELECT_IMAGE)
-                }
-            }
-        }
+        Dialog(context, registry).show(callback)
     }
 }

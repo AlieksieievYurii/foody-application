dc68f291bf4682c713a531eb5fa262ca90277706
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-07-11 23:19:03 +0200

    Add loading UserRoleRequests

--- app/src/main/java/com/yurii/foody/screens/admin/requests/RoleRequestsViewModel.kt
+++ app/src/main/java/com/yurii/foody/screens/admin/requests/RoleRequestsViewModel.kt
@@ -2,8 +2,30 @@ package com.yurii.foody.screens.admin.requests
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class RoleRequestsViewModel(private val userRoleRepository: UserRoleRepository) : ViewModel() {
+
+    private val _userRolesRequests: MutableStateFlow<PagingData<UserRoleRequest>> = MutableStateFlow(PagingData.empty())
+    val userRolesRequests: StateFlow<PagingData<UserRoleRequest>> = _userRolesRequests
+
+    init {
+        loadUserRolesRequests()
+    }
+
+    private fun loadUserRolesRequests() {
+        viewModelScope.launch {
+            userRoleRepository.getUnconfirmedUserRolesPager().cachedIn(viewModelScope).collectLatest {
+                _userRolesRequests.value = it
+            }
+        }
+    }
 
     class Factory(private val userRoleRepository: UserRoleRepository) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
--- app/src/main/java/com/yurii/foody/screens/admin/requests/UserRoleRepository.kt
+++ app/src/main/java/com/yurii/foody/screens/admin/requests/UserRoleRepository.kt
@@ -1,8 +1,15 @@
 package com.yurii.foody.screens.admin.requests
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import com.yurii.foody.api.Service
 
 class UserRoleRepository private constructor(private val service: Service) {
+
+    private val pagingConfig = PagingConfig(pageSize = 10, enablePlaceholders = false)
+
+    fun getUnconfirmedUserRolesPager() =
+        Pager(config = pagingConfig, pagingSourceFactory = { UserRoleRequestPagingSource(service) }).flow
 
     companion object {
         private var INSTANCE: UserRoleRepository? = null

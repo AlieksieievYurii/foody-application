c1d71af0b97f6f88271704c36b42b5807698b8fa
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-07-16 19:55:20 +0200

    Add handling navigation menu

--- app/src/main/java/com/yurii/foody/screens/client/main/ClientMainScreenFragment.kt
+++ app/src/main/java/com/yurii/foody/screens/client/main/ClientMainScreenFragment.kt
@@ -4,16 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.yurii.foody.R
 import com.yurii.foody.databinding.FragmentNavigationClientPanelBinding
+import com.yurii.foody.utils.OnBackPressed
 
-class ClientMainScreenFragment : Fragment() {
+class ClientMainScreenFragment : Fragment(), OnBackPressed {
     private lateinit var binding: FragmentNavigationClientPanelBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_navigation_client_panel, container, false)
+        binding.content.openMenu.setOnClickListener { binding.drawerLayout.openDrawer(GravityCompat.START) }
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_personal_information -> {
+                }
+                R.id.item_help -> {
+                }
+                R.id.item_change_role -> {
+                }
+                R.id.item_log_out -> {
+                }
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            false
+        }
+
         return binding.root
+    }
+
+    override fun onBackPressed(): Boolean {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            return true
+        }
+        return false
     }
 }

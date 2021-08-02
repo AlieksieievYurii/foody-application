b258d4542fbe90600225e61cd8885adc4fd2d61f
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-08-02 21:43:19 +0200

    Add dialog to ask user to confirm changing order status

--- app/src/main/java/com/yurii/foody/screens/cook/execution/OrderExecutionFragment.kt
+++ app/src/main/java/com/yurii/foody/screens/cook/execution/OrderExecutionFragment.kt
@@ -6,9 +6,13 @@ import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.yurii.foody.R
+import com.yurii.foody.api.OrderExecutionStatus
 import com.yurii.foody.databinding.FragmentOrderExecutionBinding
 import com.yurii.foody.utils.Injector
+import com.yurii.foody.utils.closeFragment
+import com.yurii.foody.utils.observeOnLifecycle
 
 class OrderExecutionFragment : Fragment(R.layout.fragment_order_execution) {
     private val args: OrderExecutionFragmentArgs by navArgs()
@@ -20,5 +24,38 @@ class OrderExecutionFragment : Fragment(R.layout.fragment_order_execution) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.orderStatus.setListener(viewModel::onOrderStatusChanged)
+        binding.orderStatus.observeLoading(viewModel.changingOrderStatus, viewLifecycleOwner)
+        binding.takeOrder.setOnClickListener { askUserToConfirm(viewModel::takeOrder) }
+        observeEvents()
+    }
+
+    private fun askUserToAcceptChangingStatus(currentStatus: OrderExecutionStatus, nextOrderStatus: OrderStatusComponent) {
+        MaterialAlertDialogBuilder(requireContext()).setTitle(R.string.label_change_order_status)
+            .setMessage(getString(R.string.message_change_order_status, currentStatus.status, nextOrderStatus))
+            .setPositiveButton(R.string.label_yes) { _, _ -> viewModel.performChangingOrderStatus() }
+            .setNegativeButton(R.string.label_no) { _, _ -> }
+            .show()
+    }
+
+    private fun askUserToConfirm(callback: () -> Unit) {
+        MaterialAlertDialogBuilder(requireContext()).setTitle(R.string.label_taking_order)
+            .setMessage(R.string.message_take_order_confirmation)
+            .setPositiveButton(R.string.label_yes) { _, _ -> callback.invoke() }
+            .setNegativeButton(R.string.label_no) { _, _ -> }
+            .show()
+    }
+
+    private fun observeEvents() {
+        viewModel.eventFlow.observeOnLifecycle(viewLifecycleOwner) {
+            when (it) {
+                is OrderExecutionViewModel.Event.AskUserToChangeOrderExecutionStatus -> askUserToAcceptChangingStatus(
+                    it.currentStatus,
+                    it.nextOrderStatus
+                )
+                OrderExecutionViewModel.Event.CloseScreen -> closeFragment()
+                is OrderExecutionViewModel.Event.ShowError -> TODO()
+            }
+        }
     }
 }
